7aa3788ceb114cf5d0429f14f049ae5a37a2d7e3
michalm57 <mroczekmichal57@gmail.com>
2023-07-23 10:56:29 +0200

    create contacts table and sql with inserting example records

---
+++ db/insert_into_contacts.sql
@@ -0,0 +1,5 @@
+INSERT INTO `contacts` (`id`, `firstname`, `lastname`, `email`, `updated_at`, `created_at`)
+VALUES (NULL, 'Jan', 'Kowalski', 'kowalskijan@exampledomain.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO `contacts` (`id`, `firstname`, `lastname`, `email`, `updated_at`, `created_at`)
+VALUES (NULL, 'Kamil', 'Nowak', 'nowakkamil@exampledomain.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
